9914bba98495de3de22813c66bfacad8f7dd23fa
priya7up <priya7up@gmail.com>
2016-12-09 12:12:57 -0800

    Update shift_image.py

--- shift_image.py
+++ shift_image.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-# READ IN DATA FROM CSV
 print 'Reading in training data \n'
 training_data = pd.read_csv('train.csv')
 
-# HELPER FUNCTION TO SHIFT IMAGE BY ONE COLUMN TO THE LEFT OR TO THE RIGHT
+# Helper function to shift image by one column to the left or to the right
 def imageshifterbyonecolumn(data, to_pixel, from_pixel):
     print 'Shifting image by one column from {} to {} followed by subsequent columns'.format(from_pixel, to_pixel)
     shifted_data = pd.DataFrame(columns=data.columns.values) #new dataframe that will contain data of the shifted image
@@ -15,14 +14,13 @@ def imageshifterbyonecolumn(data, to_pixel, from_pixel):
     return shifted_data
 
 
-# FUNCTION TO CREATE ONE CONCATENATED DATAFRAME CONTAINING THE RAW DATA AND NEW DATA OF THE SHIFTED IMAGES
+# Function to create one concatenated dataframe containing the raw data and new data from the shifted images
 def shift_image():
     left_shifted_training_data = imageshifterbyonecolumn(training_data, 'pixel783', 'pixel0') #shifting images by one column to the left
     right_shifted_training_data = imageshifterbyonecolumn(training_data, 'pixel0', 'pixel783') #shifting images by one column to the right
     return pd.concat([training_data, left_shifted_training_data, right_shifted_training_data]) #concatenating the three dataframes
 
 
-# WRITING THE NEW DATAFRAME INTO A CSV FILE.
 filename = 'processed_train.csv'
 print 'Writing processed training data into {}'.format(filename)
 final_training_data = shift_image()
