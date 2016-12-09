b13bf608b2ee34f19820bae13d7ea5d424255fe9
priya7up <priya7up@gmail.com>
2016-12-09 14:19:25 -0800

    Update shift_image.py

--- shift_image.py
+++ shift_image.py
@@ -1,17 +1,10 @@
 import pandas as pd
 import logging
 
-def main():
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
-    logging.info('Reading in training data \n')
-    training_data = pd.read_csv('train.csv')
-
-    # Helper function to shift image by one column to the left or to the right
-    def create_shifted_image(data, to_pixel, from_pixel):
-        logging.info('Shifting image by one column from {} to {} followed by subsequent columns'.format(from_pixel, to_pixel))
-        """ Creating a new dataframe that will contain data of the shifted image, 
-            the number of pixels in the shifted image is the same as the original image """
-        shifted_data = pd.DataFrame(columns=data.columns.values)
+def create_shifted_image(data, to_pixel, from_pixel):
+        """ Helper function to shift image by one column to the left or to the right """
+        logging.info('Shifting image by one column from %s to %s followed by subsequent columns',from_pixel, to_pixel)
+        shifted_data = pd.DataFrame(columns=data.columns.values) #Creating a new dataframe that will contain data of the shifted image, the number of pixels in the shifted image is the same as the original image
         shifted_data['label'] = data['label'] 
         shifted_data[shifted_data.columns.difference(['label', to_pixel])] = \
             data[data.columns.difference(['label', from_pixel])] 
@@ -19,18 +12,21 @@ def main():
         return shifted_data
 
 
-    # Function to create one concatenated dataframe containing the raw data and new data from the shifted images
-    def concatenate_data():
-        """ Shifting images by one column to the left """
-        left_shifted_training_data = imageshifterbyonecolumn(training_data, 'pixel783', 'pixel0') 
-        """ Shifting images by one column to the right """
-        right_shifted_training_data = imageshifterbyonecolumn(training_data, 'pixel0', 'pixel783') 
-        return pd.concat([training_data, left_shifted_training_data, right_shifted_training_data]) 
+def concatenate_data(data):
+    """ Function to create one concatenated dataframe containing the raw data and new data from the shifted images """
+    left_shifted_training_data = create_shifted_image(data, 'pixel783', 'pixel0') #Shifting images by one column to the left
+    right_shifted_training_data = create_shifted_image(data, 'pixel0', 'pixel783') #Shifting images by one column to the right
+    return pd.concat([data, left_shifted_training_data, right_shifted_training_data]) 
 
 
+def main():
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    logging.info('Reading in training data \n')
+    training_data = pd.read_csv('train.csv')
+    
     filename = 'processed_train.csv'
     logging.info('Writing processed training data into {}'.format(filename))
-    final_training_data = shift_image()
+    final_training_data = concatenate_data(training_data)
     final_training_data.to_csv(filename, index=False)
 
 if __name__ == '__main__':
