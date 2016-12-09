a49e7a870f545430082971c3dc3e2d2c817bf15d
priya7up <priya7up@gmail.com>
2016-12-09 13:23:55 -0800

    Update shift_image.py

--- shift_image.py
+++ shift_image.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging
 
 print 'Reading in training data \n'
 training_data = pd.read_csv('train.csv')
@@ -7,9 +8,9 @@ training_data = pd.read_csv('train.csv')
 def imageshifterbyonecolumn(data, to_pixel, from_pixel):
     print 'Shifting image by one column from {} to {} followed by subsequent columns'.format(from_pixel, to_pixel)
     shifted_data = pd.DataFrame(columns=data.columns.values) #new dataframe that will contain data of the shifted image
-    shifted_data['label'] = data['label'] #copying over the label from the original images
+    shifted_data['label'] = data['label'] 
     shifted_data[shifted_data.columns.difference(['label', to_pixel])] = \
-        data[data.columns.difference(['label', from_pixel])] #moving the pixel values by one column
+        data[data.columns.difference(['label', from_pixel])] #shifting the pixel values by one column
     shifted_data.loc[:, to_pixel] = data.loc[:, from_pixel] #wrapping around the 'from_pixel' values into the 'to_pixel' column
     return shifted_data
 
@@ -18,7 +19,7 @@ def imageshifterbyonecolumn(data, to_pixel, from_pixel):
 def shift_image():
     left_shifted_training_data = imageshifterbyonecolumn(training_data, 'pixel783', 'pixel0') #shifting images by one column to the left
     right_shifted_training_data = imageshifterbyonecolumn(training_data, 'pixel0', 'pixel783') #shifting images by one column to the right
-    return pd.concat([training_data, left_shifted_training_data, right_shifted_training_data]) #concatenating the three dataframes
+    return pd.concat([training_data, left_shifted_training_data, right_shifted_training_data]) 
 
 
 filename = 'processed_train.csv'
